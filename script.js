a08b1e146f9c2b3d6f50816eacd65bcf88e9d52c
Bariya Vishwas <bariyavasu46@gmail.com>
2025-06-17 23:50:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
-	const uniqueArr = [...new Set(arr)]; // Remove duplicates
-    if (uniqueArr.length < 2) return null;
-    uniqueArr.sort((a, b) => b - a);     // Descending sort
-  
-	return uniqueArr[1]; 
-};
+	let ans = [];
+
+	let left = 0;
+	let sum = 0;
+	let right = 0;
+	for(right = 0; right < arr.length; right++) {
+		sum += arr[right];
+		if(sum > n) {
+			let temp = [];
+			while(left < right) {
+				temp.push(arr[left]);
+				sum -= arr[left];
+				left++;
+			}
+			if(temp.length > 0)
+				ans.push(temp);
+		}
+	}
+
+	while(left < arr.length) {
+		let temp = [];
+		while(left < right) {
+			temp.push(arr[left++]);
+		}
+		if(temp.length > 0)
+			ans.push(temp);
+	}
+
+	return ans;
+	
+}
 
 const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
