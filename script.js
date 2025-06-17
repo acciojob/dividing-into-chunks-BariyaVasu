7b6e61dbfd3490ada4b92cd8003a04b2397b3911
Bariya Vishwas <bariyavasu46@gmail.com>
2025-06-17 23:28:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	const uniqueArr = [...new Set(arr)]; // Remove duplicates
+    if (uniqueArr.length < 2) return null;
+    uniqueArr.sort((a, b) => b - a);     // Descending sort
+  
+	return uniqueArr[1]; 
 };
 
 const n = prompt("Enter n: ");
